fde03d3067dabc787c6ecf61d9ef4614c5f6e793
Ankur8903 <153534229+Ankur8903@users.noreply.github.com>
2023-12-12 08:42:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['7719234372', 'ankur bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
